83fa96c88c0a3f7df2f1f5251d1f19031e065342
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-04 18:39:16 +0300

    Clear up dist folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'Amejid'], ' ');
   element.classList.add('hello');
 
   return element;
