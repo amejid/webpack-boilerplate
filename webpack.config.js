a71b317c0047ffa13a6a5533c08e05d8a8b3c182
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-04 18:35:11 +0300

    Setup development server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -12,6 +16,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
